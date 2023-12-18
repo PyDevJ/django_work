921a85fe936952ce449b0796b2d63588800a1e3e
PyDevJ <gitcloudhab@gmail.com>
2023-12-18 23:17:01 +0300

    application: catalog

---
+++ catalog/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+
+
+def home(request):
+    return render(request, 'catalog/home.html')
+
+
+def contacts(request):
+    if request.method == 'POST':
+        # в переменной request хранится информация о методе, который отправлял пользователь
+        name = request.POST.get('name')
+        telephone = request.POST.get('phone')
+        message = request.POST.get('message')
+        # а также передается информация, которую заполнил пользователь
+        print(name, telephone, message)
+    return render(request, 'catalog/contacts.html')
