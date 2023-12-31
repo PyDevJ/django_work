e0d526c18e888c10595d7ffa42ba17b9bf15006b
PyDevJ <gitcloudhab@gmail.com>
2023-12-31 23:35:20 +0300

    lesson 21.1, task_3

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,7 @@
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
 from django.views.generic import CreateView, ListView, DetailView, UpdateView, DeleteView
+from pytils.translit import slugify
+
 from blog.models import Blog
 
 
@@ -8,20 +10,50 @@ class BlogCreateView(CreateView):
     fields = ('title', 'text',)
     success_url = reverse_lazy('blog:list')
 
+    def form_valid(self, form):
+        if form.is_valid():
+            new_bl = form.save()
+            new_bl.slug = slugify(new_bl.title)
+            new_bl.save()
+
+        return super().form_valid(form)
+
 
 class BlogUpdateView(UpdateView):
     model = Blog
     fields = ('title', 'text',)
-    success_url = reverse_lazy('blog:list')
+    # success_url = reverse_lazy('blog:list')
+
+    def form_valid(self, form):
+        if form.is_valid():
+            new_bl = form.save()
+            new_bl.slug = slugify(new_bl.title)
+            new_bl.save()
+
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse('blog:view', args=[self.kwargs.get('pk')])
 
 
 class BlogListView(ListView):
     model = Blog
     paginate_by = 4
 
+    def get_queryset(self, *args, **kwargs):
+        queryset = super().get_queryset(*args, **kwargs)
+        queryset = queryset.filter(published=True)
+        return queryset
+
 
 class BlogDetailView(DetailView):
     model = Blog
+
+    def get_object(self, queryset=None):
+        self.object = super().get_object(queryset)
+        self.object.views += 1
+        self.object.save()
+        return self.object
 
 
 class BlogDeleteView(DeleteView):
